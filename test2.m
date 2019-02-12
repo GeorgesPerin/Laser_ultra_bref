f2915590b3a81e5b98edbe4dd61cb7722bf4b659
GeorgesPerin <47337431+GeorgesPerin@users.noreply.github.com>
2019-02-12 18:07:45 +0100

    final test 2

--- test2.m
+++ test2.m
@@ -1,5 +1,6 @@
-
+close all
  [t,u1,uplot]=Mod_lock_actif();
+ close
  figure
  hold on
  plot(t*10^(12),uplot(1,:),'ko')
@@ -11,3 +12,20 @@
  legend('impulsion initiale','impulsion modelisee','impulsion theorique')
  xlabel('Temps (ps)')
  ylabel('Champ (V.m^{-1})')
+ title('Blocage de mode actif')
+ 
+ figure
+ [~,Upas]=hme_ssfm2();
+ close
+ figure 
+ hold on
+ plot(t*10^(12),Upas(1,:),'bo')
+ plot(t*10^(12),Upas(end,:),'-rs');
+ grid on
+ set(gca,'fontsize',15)
+ box on
+ legend('impulsion initiale','impulsion modelisee')
+ xlabel('Temps (ps)')
+ ylabel('Champ (V.m^{-1})')
+ title('Blocage de mode passif')
+ 
