78878d4c0736d0606f41e87fed588f471bd10939
GeorgesPerin <47337431+GeorgesPerin@users.noreply.github.com>
2019-02-13 15:31:46 +0100

    test amélioré

--- test.m
+++ test.m
@@ -1,22 +1,24 @@
 close all 
-clear all
 
-[~,Upas]=hme_ssfm2();
-[t,~,Uact]=Mod_lock_actif();
-U1=(Upas(end,:));
-U2=(Uact(end,:));
+% [~,Upas]=hme_ssfm2();
+% [t,~,Uact]=Mod_lock_actif();
+% U1=(Upas(end,:));
+% U2=(Uact(end,:));
 plot(t,abs(U1).^2)
 hold on
 plot(t,abs(U2).^2,'-r')
-legend('PAssif','Actif')
+legend('Passif','Actif')
 xlabel('temps')
 ylabel('Puissance')
 
-fid = fopen('test.txt','w');
-fprintf(fid,'%f %f',t);
-fprintf(fid,'\n');
-fprintf(fid,'%f %f',U1);
-fprintf(fid,'\n');
-fprintf(fid,'%f %f',U2);
-fclose(fid)
+% t=t';
+% fid = fopen('test.txt','w');
+% fprintf(fid,'%f %f',t); 
+% fprintf(fid,'\n');
+% fprintf(fid,'%f %f',U1);
+% fprintf(fid,'\n');
+% fprintf(fid,'%f %f',U2);
+% fclose(fid)
+% 
 
+save('Impulsion.mat','t','U1','U2')
